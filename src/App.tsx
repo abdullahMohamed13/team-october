3449b1b47f036b6c265a614f249ead74a2648fbd
abdullahMohamed13 <https://github.com/abdullahMohamed13>
2025-10-26 22:21:46 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+function App() {
+
+  return (
+    <main className='mt-10 flex items-center gap-3 justify-center'>
+      <img src="/logo.svg" alt="" />
+      <div>
+        Hello TEAM 👋
+      </div>
+    </main>
+  )
+}
+
+export default App
+
+// used commands:
+{
+
+  /*
+    npm install react-router-dom
+
+    npm install yup
+    npm install @reduxjs/toolkit react-redux
+
+
+    // Tailwindcss / shadcn
+    npm install tailwindcss @tailwindcss/vite
+    npm install -D @types/node
+    npx shadcn@latest init
+  */
+
+}
